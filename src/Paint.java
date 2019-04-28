94a9bf7a34a77dac39e202234828038b5482bc83
Lamb <lydonjr@gmail.com>
2019-04-28 13:29:03 -0400

    Added brush sizes. changed icons for brush, eraser, clear.

--- src/Paint.java
+++ src/Paint.java
@@ -8,12 +8,14 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
+import javafx.scene.control.Separator;
 import javafx.scene.control.ToolBar;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-
 
 public class Paint extends Application {
 
@@ -25,9 +27,8 @@ public class Paint extends Application {
     private Canvas canvas;
     private GraphicsContext gc;
 
-    private Button drawButton, eraseButton, clearButton;
+    private Button drawButton, eraserButton, clearButton, lineThin, lineRgular, lineThick;
     private boolean draw = true;
-
 
     private double x, y;
     private double size = 4;
@@ -57,29 +58,77 @@ public class Paint extends Application {
         gc = canvas.getGraphicsContext2D();
         root.getChildren().add(canvas);
 
-        ToolBar leftToolbar = new ToolBar();
-        leftToolbar.setOrientation(Orientation.VERTICAL);
-        leftToolbar.prefHeightProperty().bind(primaryStage.heightProperty());
+        ToolBar leftToolbarColOne = new ToolBar();
+        leftToolbarColOne.setOrientation(Orientation.VERTICAL);
+        leftToolbarColOne.prefHeightProperty().bind(primaryStage.heightProperty());
+        leftToolbarColOne.setPrefWidth(40);
 
-        drawButton = new Button("Draw");
-        leftToolbar.getItems().add(drawButton);
-        eraseButton = new Button("Erase");
-        leftToolbar.getItems().add(eraseButton);
-        clearButton = new Button("Clear");
-        leftToolbar.getItems().add(clearButton);
+        ToolBar leftToolbarColTwo = new ToolBar();
+        leftToolbarColTwo.setOrientation(Orientation.VERTICAL);
+        leftToolbarColTwo.prefHeightProperty().bind(primaryStage.heightProperty());
+        leftToolbarColTwo.setPrefWidth(40);
 
-        VBox leftBar = new VBox(leftToolbar);
-        leftBar.setMinWidth(100);
-        leftBar.setMinHeight(DEFAULT_HEIGHT);
-        root.getChildren().add(leftBar);
+        Image drawIcon = new Image(getClass().getResourceAsStream("res/brush.png"));
+        drawButton = new Button();
+        drawButton.setMaxWidth(10);
+        drawButton.setStyle("-fx-background-color: #FEFEFE");
+        drawButton.setGraphic(new ImageView(drawIcon));
+
+        Image eraserIcon = new Image(getClass().getResourceAsStream("res/eraser.png"));
+        eraserButton = new Button();
+        eraserButton.setStyle("-fx-background-color: #FEFEFE");
+        eraserButton.setGraphic(new ImageView(eraserIcon));
+
+        Image clearIcon = new Image(getClass().getResourceAsStream("res/clear.png"));
+        clearButton = new Button();
+        clearButton.setStyle("-fx-background-color: #FEFEFE");
+        clearButton.setGraphic(new ImageView(clearIcon));
+
+        Image lineOptionThin = new Image(getClass().getResourceAsStream("res/line1.png"));
+        Image lineOptionRegular = new Image(getClass().getResourceAsStream("res/line2.png"));
+
+        lineThin = new Button();
+        lineThin.setStyle("-fx-background-color: #FEFEFE");
+        lineThin.setGraphic(new ImageView(lineOptionThin));
+
+        lineRgular = new Button();
+        lineRgular.setStyle("-fx-background-color: #FEFEFE");
+        lineRgular.setGraphic(new ImageView(lineOptionRegular));
+
+        leftToolbarColOne.getItems().add(drawButton);
+        leftToolbarColOne.getItems().add(eraserButton);
+        leftToolbarColOne.getItems().add(clearButton);
+        leftToolbarColOne.getItems().add(new Separator());
+
+        leftToolbarColOne.getItems().add(lineThin);
+        leftToolbarColOne.getItems().add(lineRgular);
+        leftToolbarColOne.getItems().add(new Separator());
+
+        VBox leftBarColOne = new VBox(leftToolbarColOne);
+        leftBarColOne.setSpacing(5);
+        leftBarColOne.setMinHeight(DEFAULT_HEIGHT);
+        root.getChildren().add(leftBarColOne);
+
+
+        //todo - fix the second toolbar so that it doesn't interfere with the buttons in the first one
+//        VBox tmp = new VBox();
+//        tmp.setTranslateX(300);
+//        tmp.setPrefWidth(100);
+
 
     }
 
     private void getMouseEvents(Canvas canvas, GraphicsContext gc) {
 
         drawButton.setOnMouseClicked(event -> draw = true);
-        eraseButton.setOnMouseClicked(event -> draw = false);
+        eraserButton.setOnMouseClicked(event -> draw = false);
         clearButton.setOnMouseClicked(event -> gc.clearRect(0, 0, DEFAULT_WIDTH, DEFAULT_HEIGHT));
+
+        lineThin.setOnMouseClicked(event -> {
+            if (size > 5) size -= 3;
+        });
+
+        lineRgular.setOnMouseClicked(event -> size += 3);
 
         canvas.setOnMouseDragged(event -> {
             x = event.getX();
@@ -87,9 +136,9 @@ public class Paint extends Application {
 
             if (draw) {
                 gc.setFill(Color.RED);
-                gc.fillRect(x, y, size, size);
+                gc.fillOval(x - size / 2, y - size / 2, size, size);
             } else {
-                gc.clearRect(x, y, size, size);
+                gc.clearRect(x - size / 2, y - size / 2, size, size);
             }
         });
 
